3be8642a7faf069eeec978d5e29e366bd5cd9989
amendomariestositinjak <amendo.sitinjak@halodoc.com>
2023-04-27 00:49:55 +0700

    fix: clean up with gitignore

--- book-service/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO book(id, isbn, title, author)
-VALUES (1, 'ISBN-123', 'Rest API with Spring Boot MVC', 'Amendo'),
-       (2, 'ISBN-134', 'Microservice with Spring Boot', 'Amendo');
--- rating-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO rating(book_id, stars) VALUES
-('ISBN-123', 5),
-('ISBN-134', 4);
